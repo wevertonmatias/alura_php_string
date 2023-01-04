ac4b44fd0ffc9b8feac20e080b0a8b97949b8129
Weverton Matias <weverton-matias@hotmail.com>
2023-01-04 17:16:43 -0300

    Avançando com caracter especial com mbstring.

--- 02_rec_string.php
+++ 02_rec_string.php
@@ -12,6 +12,13 @@ var_dump(substr($email, 0, $index));
 var_dump(substr($email, $index+1));
 
 $senha = 'abc';
-$senha2 = 'àçã';
+$senha2 = 'àçãabc';
 var_dump(strlen($senha));
 var_dump(strlen($senha2));
+var_dump(strtoupper($senha2));
+//mbstring para solucionar caracter especial
+//habilitar a extenção mb_string
+
+var_dump(mb_strlen($senha));
+var_dump(mb_strlen($senha2));
+var_dump(mb_strtoupper($senha2));
