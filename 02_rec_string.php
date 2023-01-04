c45754a378701c4939aa3a31fc17e14c613edcb8
Weverton Matias <weverton-matias@hotmail.com>
2023-01-04 14:48:59 -0300

    Encontrando caracter dentro de string.

--- 02_rec_string.php
+++ 02_rec_string.php
@@ -4,3 +4,9 @@ $email = 'vinucius@email.com.br';
 
 var_dump(substr($email,0,8 ));
 var_dump((substr($email, 9)));
+
+$email = 'Paulo@alura.com.br';
+$index = strpos($email, '@');
+
+var_dump(substr($email, 0, $index));
+var_dump(substr($email, $index+1));
