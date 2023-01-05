ef6d1d86c42607aae8560895d3430754d4978525
Weverton Matias <weverton-matias@hotmail.com>
2023-01-05 12:44:43 -0300

    String, convertendo array de strings para funcionar com echo.

--- 03_dividindo_unindo.php
+++ 03_dividindo_unindo.php
@@ -11,3 +11,15 @@ var_dump($nome, $sobrenome);
 $csv = 'Paulo Silva, paulo@gmail.com, 32';
 
 var_dump(explode(',', $csv));
+
+
+$telefones = ['82 98899-5599', '81 95566-8899', '11 95599-4499'];
+
+//echo $telefones; erro não pode converter para string
+echo implode(PHP_EOL, $telefones);
+echo implode(', ', $telefones);
+
+$nomeCompleto = 'José Felisbesto da Silva Santos';
+
+var_dump(explode(' ', $nomeCompleto, 2));
+var_dump(explode(' ', $nomeCompleto, 3));
