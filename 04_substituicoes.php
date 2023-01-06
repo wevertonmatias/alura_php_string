0243db8db2c114476c16350ab675ee03ff707560
Weverton Matias <weverton-matias@hotmail.com>
2023-01-06 14:17:55 -0300

    Strings substiruicoes caracter a caracter.

---
+++ 04_substituicoes.php
@@ -0,0 +1,17 @@
+<?php
+
+$texto = 'Texto com qualquer coisa poxa e caramba';
+
+echo str_replace(
+    ['poxa', 'caramba'],
+    '***',
+    $texto).PHP_EOL;
+
+echo str_replace(
+    ['poxa', 'caramba'],
+    ['p', 'c'],
+    $texto).PHP_EOL;
+
+echo strtr($texto, 'poxa', '***@').PHP_EOL;
+
+echo strtr($texto, ['poxa' => 'p', 'caramba' =>'c']).PHP_EOL;
---
+++ 04_substituicoes_doc.php
@@ -0,0 +1,9 @@
+<?php
+
+$trans = ["hello" => "hi", "hi" => "hello"];
+
+echo strtr("hi all, I said hello", $trans).PHP_EOL;
+
+echo str_replace(["hello", "hi"], ["hi", "hello"],"hi all, I said hello" ).PHP_EOL;
+
+echo strtr('Educação', 'çã', 'ca');
