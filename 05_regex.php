1e83754406063028c51175dafababc8a89f8ac78
Weverton Matias <weverton-matias@hotmail.com>
2023-01-09 13:01:13 -0300

    Regex substituindo string.

--- 05_regex.php
+++ 05_regex.php
@@ -1,13 +1,19 @@
 <?php
     $telefones = ['(82) 98899 - 5599', '(81) 95566 - 8899', '(11) 95599 - 4499'];
-
+    $regex = '/^\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})$/';
     foreach($telefones as $telefone){
         $telefoneValido = preg_match(
-            '/^\([0-9]{2}\) 9?[0-9]{4} - [0-9]{4}$/',
+            $regex,
             $telefone);
         if($telefoneValido){
             echo 'Telefone válido' . PHP_EOL;
         }else{
             echo 'Telefone inválido' . PHP_EOL;
         }
+
+        echo preg_replace(
+            $regex,
+            '(xx) \2',
+            $telefone).PHP_EOL;
+
     }
